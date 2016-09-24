1c22dcec6dc068a5d47feffd0cabdcf39404027e
Lorenzo Sinisi <lasslo.net@gmail.com>
2016-09-24 10:44:15 +0200

    Test configuration class

--- lib/coss/style/configuration.rb
+++ lib/coss/style/configuration.rb
@@ -3,8 +3,9 @@ module Coss
   module Style
     class Configuration
       attr_accessor :css_path, 
-                    :strict_mode
-                    :coss_configs
+                    :strict_mode,
+                    :extras
+
     end
   end
 end
---
+++ spec/coss/configuration_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe Coss::Style::Configuration do
+  
+  context 'with a configuration' do
+    before do
+     Coss::Style.configure do |config| 
+        config.css_path    = 'fixtures/coss_folder'
+        config.strict_mode = true
+        config.extras      = { something: true }
+      end
+    end
+    
+    it "has the css_path set" do
+      expect(Coss::Style.configuration.css_path).to eq('fixtures/coss_folder')
+    end
+
+    it "has the strict_mode set" do
+      expect(Coss::Style.configuration.strict_mode).to be_truthy
+    end
+
+    it "has the coss_configs set" do
+      expect(Coss::Style.configuration.extras).to eq({ something: true })
+    end
+
+  end
+
+end
